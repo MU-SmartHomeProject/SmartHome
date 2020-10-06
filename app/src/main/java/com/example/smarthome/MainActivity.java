eafbe63203b409040e226d8065e4ac55e340591e
Ryan Draper <ryandraper26@outlook.com>
2020-10-06 17:58:12 +0800

    Linking main activity to profile page activity
    using a button and intent in main activity to load profile page activity

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button profileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileBtn = (Button)findViewById(R.id.profile_page_btn);
+    }
+
+    //profileBtn has android:onclick for this function
+    public void runProfilePage(View v) {
+        Intent intent = new Intent(this, ProfilePageActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/smarthome/ProfilePageActivity.java
+++ app/src/main/java/com/example/smarthome/ProfilePageActivity.java
@@ -2,7 +2,9 @@ package com.example.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfilePageActivity extends AppCompatActivity {
 
