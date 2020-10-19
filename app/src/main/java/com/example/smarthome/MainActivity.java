ad5e4cf3e519f5270e44eaf6f8d72d89a3efe112
Rijan Chapagain <hrijon.chp@gmail.com>
2020-10-19 08:48:22 +0800

    Updates nav drawer

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.smarthome;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -8,6 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,15 +30,35 @@ public class MainActivity extends AppCompatActivity {
     private int loggedIn;
     private Button profileBtn;
     private UserDBHelper db;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         profileBtn = (Button)findViewById(R.id.profile_page_btn);
         db = new UserDBHelper(this);
         loggedIn = -1;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     //profileBtn has android:onclick for this function
