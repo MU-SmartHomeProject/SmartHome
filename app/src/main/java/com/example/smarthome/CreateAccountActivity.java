6533a5c79fc2b6a80b514454e336294f095f6d06
CodingOverload <64263162+CodingOverload@users.noreply.github.com>
2020-10-22 15:57:59 +0800

    sign up page complete

--- app/src/main/java/com/example/smarthome/CreateAccountActivity.java
+++ app/src/main/java/com/example/smarthome/CreateAccountActivity.java
@@ -2,9 +2,12 @@ package com.example.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -22,5 +25,32 @@ public class CreateAccountActivity extends AppCompatActivity {
         email = (EditText)findViewById(R.id.emailLogin);
         password = (EditText)findViewById(R.id.PasswordLogIn);
         create = (Button)findViewById(R.id.CreateBtn);
+    }
+
+    public void createAccount(View v) {
+        Cursor cursor = db.checkUsername(username.getText().toString());
+        cursor.moveToFirst();
+
+        if(cursor.getCount() == 0) {
+            cursor = db.checkEmail(email.getText().toString());
+            cursor.moveToFirst();
+
+            if(cursor.getCount() == 0){
+                if(password.getText().toString().isEmpty()) {
+                    Toast.makeText(this,"Password Required", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    db.insertData(username.getText().toString(), email.getText().toString(), password.getText().toString());
+                    Toast.makeText(this, "Account Successfully Created", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                Toast.makeText(this, "Email already exists", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else {
+            Toast.makeText(this, "Username already exists", Toast.LENGTH_SHORT).show();
+        }
     }
 }
