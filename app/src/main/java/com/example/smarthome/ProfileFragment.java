af45c96ca63342b5dd584c868db1d051c5229089
CodingOverload <64263162+CodingOverload@users.noreply.github.com>
2020-10-27 22:13:23 +0800

    update password fragment and delete

--- app/src/main/java/com/example/smarthome/ProfileFragment.java
+++ app/src/main/java/com/example/smarthome/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.smarthome;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,9 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -40,10 +44,10 @@ public class ProfileFragment extends Fragment {
         chngeEmail.setOnClickListener(handle2);
 
         chngePassword = (Button) v.findViewById(R.id.chngPass);
-        //chngePassword.setOnClickListener(this);
+        chngePassword.setOnClickListener(handle3);
 
         delAccount = (Button) v.findViewById(R.id.delAcc);
-        //delAccount.setOnClickListener(this);
+        delAccount.setOnClickListener(handle4);
 
         username = (TextView) v.findViewById(R.id.username_txt);
         email = (TextView) v.findViewById(R.id.user_email_txt);
@@ -76,6 +80,54 @@ public class ProfileFragment extends Fragment {
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragment_container, new EmailUpdateFragment(loggedIn)).commit();
+        }
+    };
+
+    View.OnClickListener handle3 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new PasswordUpdateFragment(loggedIn)).commit();
+        }
+    };
+
+    View.OnClickListener handle4 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+            alert.setCancelable(false);
+            alert.setTitle("Delete Account");
+            alert.setMessage("Are you sure you want to delete?");
+
+            alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    int deleted = db.deleteData(loggedIn);
+
+                    if(deleted > 0) {
+                        Toast.makeText(getContext(), "Account Deleted", Toast.LENGTH_SHORT).show();
+
+                        LogInPageActivity lg = new LogInPageActivity();
+                        lg.loggedOut();
+
+                        Intent intent = new Intent(getContext(), LogInPageActivity.class);
+                        startActivity(intent);
+                    }
+                    else
+                        Toast.makeText(getContext(),"Account Not Deleted", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            alert.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+
+            AlertDialog dialog = alert.create();
+            dialog.show();
         }
     };
 
