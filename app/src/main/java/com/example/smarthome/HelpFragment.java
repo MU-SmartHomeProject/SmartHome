89f15ecd3955f68b344cf14f9cdd6dfdc79257e8
Rijan Chapagain <hrijon.chp@gmail.com>
2020-10-27 15:38:09 +0800

    Updates help fragment Adds lists of steps that help to use smart home application

--- app/src/main/java/com/example/smarthome/HelpFragment.java
+++ app/src/main/java/com/example/smarthome/HelpFragment.java
@@ -8,12 +8,33 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import android.widget.AdapterView;
+import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class HelpFragment extends Fragment {
+import androidx.fragment.app.ListFragment;
+
+public class HelpFragment extends ListFragment implements OnItemClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.HelpList, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getActivity(), "Item: " + i, Toast.LENGTH_SHORT).show();
     }
 }
