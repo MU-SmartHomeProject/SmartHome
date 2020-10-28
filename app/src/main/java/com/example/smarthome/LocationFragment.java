bd7e021fad842650bda9a27f01889992534d2f59
CodingOverload <64263162+CodingOverload@users.noreply.github.com>
2020-10-28 23:01:03 +0800

    permissions added

--- app/src/main/java/com/example/smarthome/LocationFragment.java
+++ app/src/main/java/com/example/smarthome/LocationFragment.java
@@ -1,19 +1,56 @@
 package com.example.smarthome;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class LocationFragment extends Fragment {
+
+    private Button locate;
+    private EditText lat, lon;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+
+        locate = view.findViewById(R.id.locateBtn);
+        lat = view.findViewById(R.id.latTxt);
+        lon = view.findViewById(R.id.lonTxt);
+
+        return view;
     }
+
+
 }
